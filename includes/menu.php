4d0434390ee6ad08e39d5bd26fff11953cdceb44
Benoit <devunity62400@gmail.com>
2018-11-05 10:59:19 +0100

    first commit

---
+++ includes/menu.php
@@ -0,0 +1,21 @@
+﻿   <div class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <a href="index.html"><img src="images/logo.png" alt="Hair Salon Website Templates Free Download"></a>
+                </div>
+                <div class="col-lg-8 col-md-4 col-sm-12 col-xs-12">
+                    <div class="navigation">
+                        <div id="navigation">
+                            <ul>
+                                <li class="active"><a href="index.html" title="Home">Accueil</a></li>
+                                <li class=""><a href="service-list.html" title="Prendre Rendez Vous">Prendre rendez vous</a></li>
+                                <li class=""><a href="blog-default.html" title="Nos Services ">Nos Services</a></li>
+                                <li><a href="contact.html" title="Contact">Contact</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
